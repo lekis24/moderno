27803e9d16fab2355b0d87ddf16eda3531ccf2fc
lekis24 <leha.panychev@mail.ru>
2025-11-18 22:14:40 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -139,6 +139,7 @@ function images() {
 
 function fonts() {
   return src(path.src.fonts, { base: srcPath + "assets/fonts/" })
+    .pipe(dest(path.build.fonts))
     .pipe(browserSync.reload({ stream: true }))
 }
 
