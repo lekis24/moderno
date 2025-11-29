459882fb33e8e8757794cdd94d431b4aed7298ca
lekis24 <leha.panychev@mail.ru>
2025-11-29 22:22:42 +0300

    added product-slider and product

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
 
+
   var mixer = mixitup('.product__inner-box');
+
+  $(".rate-star").rateYo({
+    rating: 3.5,
+    spacing: "3px",
+    readOnly: "True",
+    starWidth: "12px",
+    numStars: 5,
+    minValue: 0,
+    maxValue: 5,
+    normalFill: 'grey',
+    ratedFill: 'orange',
+  });
+  $('.product__slider-inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
 });
